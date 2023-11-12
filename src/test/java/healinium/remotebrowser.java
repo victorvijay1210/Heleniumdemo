7d5e4901ca0eabd77963dc62de742fb46457b1f9
victorvijay1210 <70469583+victorvijay1210@users.noreply.github.com>
2023-11-12 14:09:54 +0530

    new class
    This reverts commit c0092c0b20087891cd59229acf0bf38973b8f1f6.

---
+++ src/test/java/healinium/remotebrowser.java
@@ -0,0 +1,37 @@
+package healinium;
+
+import static org.testng.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import com.epam.healenium.SelfHealingDriver;
+
+public class remotebrowser {
+	WebDriver driver;
+	
+	
+	@Test
+	public void dockertest() throws Exception {
+		URL dockerUrl = new URL("http://localhost:4444/wd/hub");
+	     ChromeOptions options = new ChromeOptions(); 
+	     RemoteWebDriver delegate = new RemoteWebDriver(dockerUrl, options);       
+		driver=	SelfHealingDriver.create(delegate);
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+		assertTrue(driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).isDisplayed());
+		driver.quit();
+	}
+	 
+
+}
